a5cbb8af02e0107672059dce65d9389e2323fe53
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-09-15 12:32:19 +0200

    Добавьте файлы проекта.

---
+++ Lab Soroka Daniil/Program.cs
@@ -0,0 +1,38 @@
+﻿//уровень Высокий 19
+
+//Console.Write("Введите a:");
+//double a = double.Parse(Console.ReadLine());
+//Console.Write("Введите y:");
+//double y = double.Parse(Console.ReadLine());
+//Console.Write("Введите r:");
+//double r = double.Parse(Console.ReadLine());
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("Введите b:");
+//double b = double.Parse(Console.ReadLine());
+//Console.Write("Введите K:");
+//double K = double.Parse(Console.ReadLine());
+//double D = (K - a * r * x - a * Math.Sqrt(6)) - Math.Cos(3 * a * b);
+//Console.WriteLine($"D={D:F2}");
+//double d = (K - a * r * x - a * Math.Sqrt(6)) - Math.Cos(3 * a * b) / Math.Pow(Math.Sin(a * Math.Asin(x) + Math.Log(y)), 2);
+//Console.WriteLine($"d={d:F2}");
+//Math.Pow(K, -a * r * x);
+
+//Уровень Высокий 19
+
+//Console.Write("Введите кординату X для 1 вершины:");
+//double X1 = double.Parse(Console.ReadLine());
+//Console.Write("Введите кординату Y для 1 вершины:");
+//double Y1 = double.Parse(Console.ReadLine());
+//Console.Write("Введите кординату X для 2 вершины:");
+//double X2 = double.Parse(Console.ReadLine());
+//Console.Write("Введите кординату Y для 2 вершины:");
+//double Y2 = double.Parse(Console.ReadLine());
+//Console.Write("Введите кординату X для 3 вершины:");
+//double X3 = double.Parse(Console.ReadLine());
+//Console.Write("Введите кординату Y для 3 вершины:");
+//double Y3 = double.Parse(Console.ReadLine());
+//double L = (Math.Pow(X2 - X1, 2) + (Math.Pow(Y2 - Y1, 2)));
+//double S = (Math.Pow(X3 - X1, 2) + (Math.Pow(Y3 - Y1, 2)));
+//double P = (Math.Pow(X2 - X3, 2) + (Math.Pow(Y2 - Y3, 2)));
+//Console.WriteLine($"Периметр = {L + S + P:F2}");
